42f920bf130fb8856a2486ea03ffc68693e47817
usuario <ingeieropauloescobar@gmail.com>
2018-03-03 17:09:52 -0500

    se creo test para producto

---
+++ aingo/back-end/aingoPos/src/test/java/dominioTest/ProductoTest.java
@@ -0,0 +1,17 @@
+package dominioTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dominio.Producto;
+
+public class ProductoTest {
+	
+	@Test
+	public void crearProducto() {
+		Producto producto = new Producto(4,"sal");
+		assertTrue(producto.getCodigo()==4);
+		assertTrue(producto.getNombre().equals("sal"));
+	}
+}
