deaf46f26fe6dfe783301c4b08e97bc6a79a1daf
usuario <ingeieropauloescobar@gmail.com>
2018-03-03 17:35:45 -0500

    se creo atributos para la clase factura

--- aingo/back-end/aingoPos/src/main/java/dominio/Factura.java
+++ aingo/back-end/aingoPos/src/main/java/dominio/Factura.java
@@ -1,4 +1,71 @@
 package dominio;
 
+import java.util.ArrayList;
+
 public class Factura {
+	private int id;
+	private double impuesto, totalNeto, propina, totalBruto;
+	private String mesa, fecha, mensajeHeader, mensajeFooter;
+	private ArrayList<Producto> productos;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getImpuesto() {
+		return impuesto;
+	}
+	public void setImpuesto(double impuesto) {
+		this.impuesto = impuesto;
+	}
+	public double getTotalNeto() {
+		return totalNeto;
+	}
+	public void setTotalNeto(double totalNeto) {
+		this.totalNeto = totalNeto;
+	}
+	public double getPropina() {
+		return propina;
+	}
+	public void setPropina(double propina) {
+		this.propina = propina;
+	}
+	public double getTotalBruto() {
+		return totalBruto;
+	}
+	public void setTotalBruto(double totalBruto) {
+		this.totalBruto = totalBruto;
+	}
+	public String getMesa() {
+		return mesa;
+	}
+	public void setMesa(String mesa) {
+		this.mesa = mesa;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public String getMensajeHeader() {
+		return mensajeHeader;
+	}
+	public void setMensajeHeader(String mensajeHeader) {
+		this.mensajeHeader = mensajeHeader;
+	}
+	public String getMensajeFooter() {
+		return mensajeFooter;
+	}
+	public void setMensajeFooter(String mensajeFooter) {
+		this.mensajeFooter = mensajeFooter;
+	}
+	public ArrayList<Producto> getProductos() {
+		return productos;
+	}
+	public void setProductos(ArrayList<Producto> productos) {
+		this.productos = productos;
+	}  
+	
 }
