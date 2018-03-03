47591b1bcd3b2261f48bbc45e2b16b68d9c23612
Paulo Escobar <ingenieropauloescobar@gmail.com>
2018-03-03 15:03:00 -0500

    GRAL: iniciar proyecto

---
+++ aingo/back-end/aingoPos/src/main/java/com/aingo/aingoPos/AingoPosApplication.java
@@ -0,0 +1,12 @@
+package com.aingo.aingoPos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AingoPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AingoPosApplication.class, args);
+	}
+}
