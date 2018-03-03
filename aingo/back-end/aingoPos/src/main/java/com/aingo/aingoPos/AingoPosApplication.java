017cdc48bab4671ee983bde30c2e15e6c36f8bcb
Paulo Escobar <ingenieropauloescobar@gmail.com>
2018-03-03 17:38:22 -0500

    cambios minimos

--- aingo/back-end/aingoPos/src/main/java/com/aingo/aingoPos/AingoPosApplication.java
+++ aingo/back-end/aingoPos/src/main/java/com/aingo/aingoPos/AingoPosApplication.java
@@ -2,8 +2,10 @@ package com.aingo.aingoPos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("persistencia.entidad")
 public class AingoPosApplication {
 
 	public static void main(String[] args) {
